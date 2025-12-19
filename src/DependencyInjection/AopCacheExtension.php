24b838684c71c87c9dc623500da89f490b7712a5
github-ci <you@example.com>
2025-12-19 17:14:47 +0800

    upgrade

--- src/DependencyInjection/AopCacheExtension.php
+++ src/DependencyInjection/AopCacheExtension.php
@@ -4,7 +4,7 @@ namespace Tourze\Symfony\AopCacheBundle\DependencyInjection;
 
 use Tourze\SymfonyDependencyServiceLoader\AutoExtension;
 
-class AopCacheExtension extends AutoExtension
+final class AopCacheExtension extends AutoExtension
 {
     protected function getConfigDir(): string
     {
